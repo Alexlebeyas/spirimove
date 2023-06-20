8b321b91fd952bc2fe6e8ed43f86dab738b16f25
Alexandre Kadian <akadian@spiria.com>
2023-06-20 10:49:28 -0400

    Moved backend to a backend folder and the webclient ui to a webclient folder

---
+++ webclient/src/pages/NotFound/index.tsx
@@ -0,0 +1,9 @@
+const NotFound = () => {
+  return (
+    <div className="flex h-screen items-center justify-center bg-slate-400">
+      <h1>Not Found</h1>
+    </div>
+  );
+};
+
+export default NotFound;
