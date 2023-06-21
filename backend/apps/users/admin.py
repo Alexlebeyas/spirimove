3ed744c3e02ecf6099502ecb5521c80dee5ce7ce
Alexis Mouloum <alexledou392@gmail.com>
2023-06-21 10:45:09 -0400

    remove comment

--- backend/apps/users/admin.py
+++ backend/apps/users/admin.py
@@ -13,7 +13,6 @@ class UserAdmin(DjangoUserAdmin):
         (None, {'fields': ('display_name', 'phone', 'password', )}),
         (_('Personal info'), {'fields': ('first_name', 'last_name')}),
         (_('Permissions'), {'fields': ('is_active', 'is_staff', 'is_superuser', 'groups')}),
-        # (_('Permissions'), {'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')}),
         (_('Important dates'), {'fields': ('last_login', 'date_joined')}),
     )
 
