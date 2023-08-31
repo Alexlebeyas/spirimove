943eab2027f79801d9051f0764ddfd80ebc2a967
Baptiste Decroix <bdecroix@spiria.com>
2023-08-31 10:41:43 -0400

    add leaderboard cumulated stats

--- webclient/src/pages/Leaderboard/index.tsx
+++ webclient/src/pages/Leaderboard/index.tsx
@@ -39,8 +39,8 @@ const Leaderboard = () => {
 
   return (
     <PageContainer>
+      {/* Office filters */}
       <div className="flex flex-col items-center justify-center">
-        {/* Office filters */}
         <ul className="mb-4 flex flex-nowrap text-center text-sm font-medium">
           {OFFICES.map((office) => (
             <li className="mr-2" key={office.id}>
@@ -56,16 +56,17 @@ const Leaderboard = () => {
           ))}
         </ul>
       </div>
-      <div className="mb-3 flex flex-col">
+      <div className="mb-3 flex flex-col lg:flex-row">
+        {/* Leaderboard */}
         <div className="mr-3 w-full lg:w-2/3">
           <div className="mb-2 max-h-[511px] rounded-md bg-white p-4 shadow-md">
             <h3 className="mb-2 text-lg font-bold">Leaderboard</h3>
             {/* Stat selection */}
             <div className="mb-2 flex justify-center">
-              <div className="rounded-3xl bg-slate-100 py-0">
+              <div className="rounded-full bg-slate-100 py-0">
                 <button
                   className={
-                    'mx-1 rounded-3xl px-4 py-1 text-xs font-medium ' +
+                    'mx-1 rounded-full px-4 py-1 text-xs font-medium ' +
                     (sortingMode == 'pts' ? 'bg-gray-700 text-slate-200' : '')
                   }
                   onClick={() => setSortingMode('pts')}
@@ -74,7 +75,7 @@ const Leaderboard = () => {
                 </button>
                 <button
                   className={
-                    'mx-1 rounded-3xl px-4 py-1 text-xs font-medium ' +
+                    'mx-1 rounded-full px-4 py-1 text-xs font-medium ' +
                     (sortingMode == 'days' ? 'bg-gray-700 text-slate-200' : '')
                   }
                   onClick={() => setSortingMode('days')}
@@ -86,6 +87,28 @@ const Leaderboard = () => {
             {/* Table */}
             <LeaderboardTable stats={stats} mode={sortingMode} officeFilter={officeFilter}></LeaderboardTable>
           </div>
+        </div>
+        <div className="mr-3 w-full lg:w-1/3">
+          {/* Cumulated stats */}
+          <div className="mb-2 w-full rounded-md bg-white p-4 shadow-md">
+            <h3 className="mb-3 text-lg font-bold">Cumulated</h3>
+            <div className="m-2 flex flex-row ">
+              <div className="flex grow justify-stretch">
+                <div className="grow p-1">
+                  <div className="justify-center text-4xl font-semibold">540</div>
+                  <div className="text-xs font-medium leading-[14px] text-gray-500">Pts</div>
+                </div>
+                <div className="grow border-l p-1 pl-7">
+                  <div className="text-4xl font-semibold">16</div>
+                  <div className="text-xs font-medium leading-[14px] text-gray-500">Participants</div>
+                </div>
+              </div>
+            </div>
+          </div>
+          {/* Spirimove progress */}
+          {/* <div className="mb-2 w-full rounded-md bg-white p-4 shadow-md">
+            <h3 className="mb-2 text-lg font-bold">Spirimove progress</h3>
+          </div> */}
         </div>
       </div>
     </PageContainer>
