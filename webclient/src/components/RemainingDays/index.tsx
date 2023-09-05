4150b5de4f8951ed34db28ddc675f2b6b81b09b6
Baptiste Decroix <bdecroix@spiria.com>
2023-09-05 16:45:50 -0400

    tweak circular progress design

--- webclient/src/components/RemainingDays/index.tsx
+++ webclient/src/components/RemainingDays/index.tsx
@@ -20,17 +20,17 @@ const RemainingDays = () => {
   return (
     <>
       <div className="m-2 flex flex-col items-center">
-        <div className="flex grow flex-col items-center">
-          <div className="absolute z-0 flex -rotate-90">
+        <div className="relative flex grow flex-col items-center">
+          <div className="absolute -bottom-[75px] z-0 flex -rotate-90">
             <CircularProgress size={150} variant="determinate" value={progressValue} />
           </div>
-          <div className=" z-1 z-0 grow items-center border-b p-6">
+          <div className="z-10 mt-4 grow items-center border-b">
             <div className="text-xs font-medium text-gray-500">Day</div>
             <div className="text-4xl font-semibold">{currentDay}</div>
           </div>
         </div>
-        <div className="text-base font-semibold ">{t('Leaderboard.RemainingDays.Stimulation')}</div>
-        <div className="text-xs font-medium text-gray-500">
+        <div className="z-10 text-base font-semibold ">{t('Leaderboard.RemainingDays.Stimulation')}</div>
+        <div className="z-10 text-xs font-medium text-gray-500">
           {t('Leaderboard.RemainingDays.ContestEnds', { contest: contest.name, remaining: remainingDays })}
         </div>
       </div>
