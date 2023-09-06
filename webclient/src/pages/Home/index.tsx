3bba3d60a0f6362e8854ec8ea8294fd8d3634f84
Baptiste Decroix <bdecroix@spiria.com>
2023-09-06 17:03:17 -0400

    fix conflict
    don't display anything in feed when there is no contest

--- webclient/src/pages/Home/index.tsx
+++ webclient/src/pages/Home/index.tsx
@@ -27,31 +27,35 @@ const Home = () => {
 
   return (
     <PageContainer>
-      <div className="flex flex-col items-center justify-center">
-        <button
-          className="mb-6 w-full rounded-md bg-gray-800 py-5 text-xl text-white hover:bg-gray-700"
-          onClick={() => setIsOpen(true)}
-        >
-          {t('Home.AddParticipation')}
-        </button>
-        {isLoading && <CircularProgress color="inherit" />}
-        <InfiniteScroll
-          dataLength={participations.length}
-          next={fetchNext}
-          hasMore={!!next}
-          loader={<CircularProgress color="inherit" />}
-          endMessage={<p>{t('Participation.NoMoreToLoad')}</p>}
-        >
-          {!isLoading && participations?.length !== 0 &&
-            participations?.map((participation) => (
-              <ParticipationCard
-                key={participation.id}
-                participation={participation}
-              />
-            ))}
-          </InfiniteScroll>
-      </div>
-      <ParticipateModal contestId={contest.id} startDate={contest.start_date} open={isOpen} setOpen={setIsOpen} />
+      {contest ? (
+        <>
+          <div className="flex flex-col items-center justify-center">
+            <button
+              className="mb-6 w-full rounded-md bg-gray-800 py-5 text-xl text-white hover:bg-gray-700"
+              onClick={() => setIsOpen(true)}
+            >
+              {t('Home.AddParticipation')}
+            </button>
+            {isLoading && <CircularProgress color="inherit" />}
+            <InfiniteScroll
+              dataLength={participations.length}
+              next={fetchNext}
+              hasMore={!!next}
+              loader={<CircularProgress color="inherit" />}
+              endMessage={<p>{t('Participation.NoMoreToLoad')}</p>}
+            >
+              {!isLoading &&
+                participations?.length !== 0 &&
+                participations?.map((participation) => (
+                  <ParticipationCard key={participation.id} participation={participation} />
+                ))}
+            </InfiniteScroll>
+          </div>
+          <ParticipateModal contestId={contest.id} startDate={contest.start_date} open={isOpen} setOpen={setIsOpen} />
+        </>
+      ) : (
+        <></>
+      )}
     </PageContainer>
   );
 };
