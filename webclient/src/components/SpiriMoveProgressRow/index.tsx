a885756f6162544de0a6d76421a95f4bfa9ac894
Praveen Venigalla <praveenv.aiesec@gmail.com>
2023-09-07 01:46:31 -0400

    SM-54 -- Add score to the table body and add logic to calculate score based on the status_display value

--- webclient/src/components/SpiriMoveProgressRow/index.tsx
+++ webclient/src/components/SpiriMoveProgressRow/index.tsx
@@ -1,6 +1,6 @@
 import { useTranslation } from 'react-i18next';
 import './index.css';
-import { IParticipation } from '@/interfaces';
+import { IParticipation, ParticipationStatus } from '@/interfaces';
 import DoneIcon from '@mui/icons-material/Done';
 import CancelIcon from '@mui/icons-material/Cancel';
 import HourglassTopIcon from '@mui/icons-material/HourglassTop';
@@ -8,16 +8,40 @@ import DeleteIcon from '@mui/icons-material/Delete';
 import EditIcon from '@mui/icons-material/Edit';
 import IconButton from '@mui/material/IconButton';
 import { Dispatch, SetStateAction} from 'react';
-import { PARTICIPATION_IN_VERIFICATION } from '@/constants';
-
 
 interface Props {
   currentDate: string;
-  participation?: IParticipation|undefined;
+  participation?: IParticipation;
   setConfirmDeleteOpen: Dispatch<SetStateAction<boolean>>;
   setIsEditParticipationModalOpen: Dispatch<SetStateAction<boolean>>;
   setParticipationToHandle: Dispatch<SetStateAction<IParticipation>>;
 }
+
+const getScore = (status: ParticipationStatus, points: number): string | number => {
+  switch (status) {
+    case ParticipationStatus.Approved:
+      return points || 0;
+    case ParticipationStatus.Rejected:
+      return 0;
+    case ParticipationStatus.InVerification:
+      return '-';
+    default:
+      return 0;
+  }
+}
+
+const renderStatusIcon = (status: ParticipationStatus) => {
+  switch (status) {
+    case ParticipationStatus.Approved:
+      return <DoneIcon style={{ color: 'green' }} />;
+    case ParticipationStatus.InVerification:
+      return <HourglassTopIcon style={{ color: 'orange' }} />;
+    case ParticipationStatus.Rejected:
+      return <CancelIcon style={{ color: 'red' }} />;
+    default:
+      return null;
+  }
+};
 
 export const SpiriMoveProgressRow: React.FC<Props> = ({ currentDate, participation, setConfirmDeleteOpen, setParticipationToHandle, setIsEditParticipationModalOpen }) => {
   const { t } = useTranslation();
@@ -36,7 +60,7 @@ export const SpiriMoveProgressRow: React.FC<Props> = ({ currentDate, participati
     }
   }
 
-  if (participation === undefined) {
+  if (!participation) {
     return (
       <tr className="flex-no wrap mb-2 flex flex-col hover:bg-gray-100 sm:mb-0 sm:table-row">
         <td className="border-grey-light border p-3">{currentDate}</td>
@@ -59,11 +83,14 @@ export const SpiriMoveProgressRow: React.FC<Props> = ({ currentDate, participati
         {participation?.type ? intensityValue : <hr className="h-px border-0 bg-gray-500" />}
       </td>
       <td className="border-grey-light border p-3">
-        {participation?.status_display === 'Approved' ? <DoneIcon style={{ color: 'green' }} /> : (participation?.status_display === 'In verification' ? <HourglassTopIcon style={{ color: 'orange' }} /> : <CancelIcon style={{ color: 'red' }} />) }
+      {renderStatusIcon(participation?.status_display)}
         {participation?.status_display }
       </td>
       <td className="border-grey-light border p-3">
-        {participation?.status_display === PARTICIPATION_IN_VERIFICATION && (
+      {getScore(participation?.status_display, participation?.points)}
+      </td>
+      <td className="border-grey-light border p-3">
+        {participation?.status_display === ParticipationStatus.InVerification && (
           <>
             <IconButton style={{ color: 'green' }} aria-label="edit" onClick={editRow}> <EditIcon /> </IconButton>
             <IconButton style={{ color: 'red' }} aria-label="delete" onClick={deleteRow}> <DeleteIcon /> </IconButton>
