582a45e11720e6791f5131077cf13cb711e76b52
Rose-Marie Bouthat <rmbouthat@spiria.com>
2023-09-08 12:06:17 -0400

    Changed the bg color of the app

--- webclient/src/components/AppContainer/index.tsx
+++ webclient/src/components/AppContainer/index.tsx
@@ -3,7 +3,7 @@ interface Props {
 }
 export const AppContainer: React.FC<Props> = ({ children }) => {
   return (
-    <div className="bg-lightergrey pb-10 pt-24 sm:pt-28">
+    <div className="bg-lightblue pb-10 pt-24 sm:pt-28">
       <div className="min-h-[75vh]">{children}</div>
     </div>
   );
