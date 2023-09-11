3644b8ea60f1e9efb8df9c1869fe950b9a053965
Rose-Marie Bouthat <rmbouthat@spiria.com>
2023-09-11 11:43:06 -0400

    adjust bg height on leaderboard & profile page

--- webclient/src/components/AppContainer/index.tsx
+++ webclient/src/components/AppContainer/index.tsx
@@ -7,7 +7,7 @@ interface Props {
 export const AppContainer: React.FC<Props> = ({ children }) => {
   return (
     <CurrentContestProvider>
-      <div className="bg-lightblue pb-10 pt-24 sm:pt-28">
+      <div className="h-screen overflow-auto bg-lightblue pb-10 pt-24 sm:pt-28">
         <div className="min-h-[75vh]">{children}</div>
       </div>
     </CurrentContestProvider>
