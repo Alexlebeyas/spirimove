fbbf8a3b455709222cacf9899ee7ec87029ac3e8
Praveen Venigalla <praveenv.aiesec@gmail.com>
2023-09-12 16:42:35 -0400

    SM-73 -- make score board pretty and add translations that support 0, 1 and plural reward levels

--- webclient/src/components/MyContestScore/index.tsx
+++ webclient/src/components/MyContestScore/index.tsx
@@ -23,27 +23,29 @@ const MyContestScore = () => {
       </div>
     );
   }
-  
+
   const rewardLevel = levels.filter(({ participation_day }) => participation_day <= stats.nb_days).length;
 
   return (
-    <div className="mb-6 w-full overflow-hidden rounded-md bg-white shadow-md">
-      <div className="block rounded-lg border border-gray-200 bg-white p-6 shadow hover:bg-gray-100 dark:border-gray-700">
-        <h6 className="mb-2 text-xl font-semibold tracking-tight text-gray-900 dark:text-white">
+    <div className="mb-6 w-full max-w-[350px] overflow-hidden rounded-md bg-white shadow-md">
+      <div className="block rounded-lg border border-gray-200 bg-white p-6 shadow">
+        <h6 className="text-xl font-semibold tracking-tight text-gray-900">
           {stats.contest_name}
         </h6>
-        <div className="flex items-center m-4">
-          <h3 className="font-medium tracking-tight text-gray-900">
-            {stats.nb_point} <span className="text-sm font-normal mr-6">{t('MySpiriMove.Stats.Points')}</span>
-          </h3>
-          <span className="text-gray-300 align-middle inline-block transform scale-y-150">|</span>
-          <h3 className="font-medium tracking-tight text-gray-900 dark:text-white ml-6">
-            {stats.nb_days} <span className="text-sm font-normal">{t('MySpiriMove.Stats.Days')}</span>
-          </h3>
+        <div className="flex flex-col justify-center items-left h-full">
+          <div className="flex items-center m-4">
+            <h3 className="font-medium tracking-tight text-gray-900">
+              {stats.nb_point} <span className="text-sm font-normal mr-7">{t('MySpiriMove.Stats.Points')}</span>
+            </h3>
+            <span className="text-gray-300 align-middle inline-block transform scale-y-150">|</span>
+            <h3 className="font-medium tracking-tight text-gray-900 ml-7">
+              {stats.nb_days} <span className="text-sm font-normal">{t('MySpiriMove.Stats.Days')}</span>
+            </h3>
+          </div>
+          <p className="ml-4 text-sm font-normal text-gray-900">
+            {t('MySpiriMove.Stats.RewardLevel', { count: rewardLevel })}
+          </p>
         </div>
-        <p className="ml-4 text-sm font-normal text-gray-900">
-          {t('MySpiriMove.Stats.RewardLevel', { rewardLevel: rewardLevel })}
-        </p>
       </div>
     </div>
   );
