6d716862b9d4cb17382a2a11a02524ba8b5f6d47
Praveen Venigalla <praveenv.aiesec@gmail.com>
2023-09-12 15:42:52 -0400

    SM-73 -- adjust score board styling to match that of the mockups

--- webclient/src/components/MyContestScore/index.tsx
+++ webclient/src/components/MyContestScore/index.tsx
@@ -24,39 +24,28 @@ const MyContestScore = () => {
       </div>
     );
   }
-  const sortedLevel = levels.sort((levelA, levelB) => levelA.participation_day - levelB.participation_day);
-  const rewardLevel = levels.filter(({ participation_day }) => participation_day <= stats.nb_days).length;
-
-  const currentEligibleLevel = sortedLevel.filter(({ participation_day }) => participation_day <= stats.nb_days);
-  const nextEligibleLevel = sortedLevel.filter(({ participation_day }) => participation_day > stats.nb_days);
   
-  const currentEligibleLevelMessage = currentEligibleLevel.length > 0 ? `Current Eligibility :  ${currentEligibleLevel.map(x => x.name+' ('+x.participation_day+' Jours, '+ x.price+')')}`:'';
-  const nextEligibleLevelMessage = nextEligibleLevel.length > 0 ? `Next level objective : ${nextEligibleLevel[0].name+' ('+nextEligibleLevel[0].participation_day+' Jours, '+ nextEligibleLevel[0].price+')' }` : '';
-
-  const streakText = stats.streak > 1 ? t('MySpiriMove.Stats.Streak', { streak: stats.streak }) : '';
+  const rewardLevel = levels.filter(({ participation_day }) => participation_day <= stats.nb_days).length;
 
   return (
     <div className="mb-6 w-full overflow-hidden rounded-md bg-white shadow-md">
-      <a
-        href="#"
-        className="block rounded-lg border border-gray-200 bg-white p-6 shadow hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700"
-      >
-        <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-          {t('MySpiriMove.Stats.Title', { name: stats.contest_name })}
-        </h5>
-        <p className="font-normal text-gray-700 dark:text-gray-400">
-          {t('MySpiriMove.Stats.Points', { points: stats.nb_point })}
-        </p>
-        <p className="font-normal text-gray-700 dark:text-gray-400">
-          {t('MySpiriMove.Stats.Days', { days: stats.nb_days })}
-        </p>
-        <p className="font-normal text-gray-700 dark:text-gray-400">{streakText}</p>
-        <p className="font-normal text-gray-700 dark:text-gray-400">
+      <div className="block rounded-lg border border-gray-200 bg-white p-6 shadow hover:bg-gray-100 dark:border-gray-700">
+        <h6 className="mb-2 text-xl font-semibold tracking-tight text-gray-900 dark:text-white">
+          {stats.contest_name}
+        </h6>
+        <div className="flex items-center m-4">
+          <h3 className="font-medium tracking-tight text-gray-900">
+            {stats.nb_point} <span className="text-sm font-normal mr-6">{t('MySpiriMove.Stats.Points')}</span>
+          </h3>
+          <span className="text-gray-300 align-middle inline-block transform scale-y-150">|</span>
+          <h3 className="font-medium tracking-tight text-gray-900 dark:text-white ml-6">
+            {stats.nb_days} <span className="text-sm font-normal">{t('MySpiriMove.Stats.Days')}</span>
+          </h3>
+        </div>
+        <p className="ml-4 text-sm font-normal text-gray-900">
           {t('MySpiriMove.Stats.RewardLevel', { rewardLevel: rewardLevel })}
         </p>
-        <p className="font-normal text-gray-700 dark:text-gray-400">{currentEligibleLevelMessage}</p>
-        <p className="font-normal text-gray-700 dark:text-gray-400">{nextEligibleLevelMessage}</p>
-      </a>
+      </div>
     </div>
   );
 };
