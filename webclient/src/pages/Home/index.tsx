391ca69ed752d469933a7b0a867dc0cb52c396d3
Praveen Venigalla <praveenv.aiesec@gmail.com>
2023-09-15 14:54:47 -0400

    SM-103 -- fix and rewrite infinite scroll component to fetch participation logs

--- webclient/src/pages/Home/index.tsx
+++ webclient/src/pages/Home/index.tsx
@@ -1,35 +1,57 @@
+import { useState, useEffect } from 'react';
+import { useTranslation } from 'react-i18next';
+import { Link } from 'react-router-dom';
+import CircularProgress from '@mui/material/CircularProgress';
+import AddIcon from '@mui/icons-material/Add';
+import ArrowForwardIcon from '@mui/icons-material/ArrowForward';
+import { useLocation } from 'react-router-dom';
+import useInfiniteScroll from 'react-infinite-scroll-hook';
+
 import { PageContainer, ParticipateModal, ParticipationCard } from '@/components';
 import { useContest } from '@/hooks';
 import { fetchAllParticipations, fetchParticipationsType } from '@/stores/useParticipationStore';
-import CircularProgress from '@mui/material/CircularProgress';
-import { useState } from 'react';
-import { useTranslation } from 'react-i18next';
-import { Link } from 'react-router-dom';
-
-import AddIcon from '@mui/icons-material/Add';
-import ArrowForwardIcon from '@mui/icons-material/ArrowForward';
-import InfiniteScroll from 'react-infinite-scroll-component';
-
 import 'react-toastify/dist/ReactToastify.css';
 
 const Home = () => {
   const { t } = useTranslation();
-  const { isLoading, participations, getParticipations, nextParticipations, next } = fetchAllParticipations(
-    (state) => state
-  );
-  const { getParticipationsTypes } = fetchParticipationsType((state) => state);
+
+  const { isLoading: isLoadingParticipationsType, getParticipationsTypes } = fetchParticipationsType();
+
+  const {
+    isLoading: isLoadingParticipations,
+    participations,
+    getParticipations,
+    nextParticipations,
+    next,
+  } = fetchAllParticipations();
+  const location = useLocation();
 
   const { contest } = useContest();
 
   const [isOpen, setIsOpen] = useState(false);
-  if (isLoading) {
-    getParticipations();
-    getParticipationsTypes();
-  }
 
   const fetchNext = async () => {
     nextParticipations(participations, next);
   };
+
+  const [sentryRef] = useInfiniteScroll({
+    loading: isLoadingParticipations,
+    hasNextPage: !!next,
+    onLoadMore: fetchNext,
+    rootMargin: '0px 0px 400px 0px',
+  });
+
+  useEffect(() => {
+    if (isLoadingParticipationsType) {
+      getParticipationsTypes();
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [isLoadingParticipationsType]);
+
+  useEffect(() => {
+    getParticipations();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [location]);
 
   const AddParticipationButton = () => (
     <button
@@ -48,7 +70,7 @@ const Home = () => {
         to="/leaderboard"
         className="text-md mb-7 flex items-center justify-center rounded-md bg-yellow px-3 py-4 font-bold antialiased hover:bg-yellow"
       >
-       {t('Home.ViewResults')} <ArrowForwardIcon className="ml-2" />
+        {t('Home.ViewResults')} <ArrowForwardIcon className="ml-2" />
       </Link>
     </div>
   );
@@ -58,22 +80,20 @@ const Home = () => {
       {contest ? (
         <>
           <div className="flex flex-col items-center justify-center">
-            {contest?.show_winners ? <ContestOverView />: <AddParticipationButton />}
+            {contest?.show_winners ? <ContestOverView /> : <AddParticipationButton />}
 
-            {isLoading && <CircularProgress color="inherit" />}
-            <InfiniteScroll
-              dataLength={participations.length}
-              next={fetchNext}
-              hasMore={!!next}
-              loader={<CircularProgress color="inherit" />}
-              endMessage={<p>{t('Participation.NoMoreToLoad')}</p>}
-            >
-              {!isLoading &&
-                participations?.length !== 0 &&
-                participations?.map((participation) => (
-                  <ParticipationCard key={participation.id} participation={participation} />
-                ))}
-            </InfiniteScroll>
+            {isLoadingParticipations && <CircularProgress color="inherit" />}
+            {!isLoadingParticipations &&
+              participations?.length !== 0 &&
+              participations.map((participation) => (
+                <ParticipationCard key={participation.id} participation={participation} />
+              ))}
+            {(isLoadingParticipations || next) && (
+              <div ref={sentryRef}>
+                <CircularProgress color="inherit" />
+              </div>
+            )}
+            {!next && <p>{t('Participation.NoMoreToLoad')}</p>}
           </div>
           <ParticipateModal contestId={contest.id} startDate={contest.start_date} open={isOpen} setOpen={setIsOpen} />
         </>
