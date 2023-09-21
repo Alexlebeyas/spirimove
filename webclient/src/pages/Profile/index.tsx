1a18d6d70b3fb19122aadaecd64cca358525257c
Alexis Mouloum <>
2023-09-21 15:19:06 -0400

    fix review comments

--- webclient/src/pages/Profile/index.tsx
+++ webclient/src/pages/Profile/index.tsx
@@ -43,7 +43,6 @@ const Profile = () => {
     setLanguage(e.target.value);
     i18next.changeLanguage(e.target.value);
     setCurrentLanguage(e.target.value);
-    window.location.reload();
   };
 
   return (
