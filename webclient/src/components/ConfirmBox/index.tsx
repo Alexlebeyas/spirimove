9cbad79ac081f5eeacd8a26ceedb430a1d3718e5
Rose-Marie Bouthat <rmbouthat@spiria.com>
2023-09-22 09:05:48 -0400

    Adjust styles on delete modal

--- webclient/src/components/ConfirmBox/index.tsx
+++ webclient/src/components/ConfirmBox/index.tsx
@@ -5,50 +5,63 @@ import DialogActions from '@mui/material/DialogActions';
 import DialogContent from '@mui/material/DialogContent';
 import DialogContentText from '@mui/material/DialogContentText';
 import DialogTitle from '@mui/material/DialogTitle';
-import { Dispatch, SetStateAction} from 'react';
+import { Dispatch, SetStateAction } from 'react';
 import { IParticipation } from '@/interfaces';
 import { useTranslation } from 'react-i18next';
-import { formatDate } from '@/utils/dates'; 
+import { formatDate } from '@/utils/dates';
 
 interface Props {
-    open: boolean;
-    setOpen: Dispatch<SetStateAction<boolean>>;
-    participation?: IParticipation | null;
-    handleDeleteParticipation: () => void;
+  open: boolean;
+  setOpen: Dispatch<SetStateAction<boolean>>;
+  participation?: IParticipation | null;
+  handleDeleteParticipation: () => void;
 }
-  
+
 export const ConfirmBox: React.FC<Props> = ({ open, setOpen, participation, handleDeleteParticipation }) => {
-    const handleClose = () => {
-        setOpen(false);
-    };
-    const handleDelete = () => {
-        handleDeleteParticipation();
-        setOpen(false);
-    };
+  const handleClose = () => {
+    setOpen(false);
+  };
+  const handleDelete = () => {
+    handleDeleteParticipation();
+    setOpen(false);
+  };
 
-    const { t } = useTranslation();
+  const { t } = useTranslation();
 
-    return (
-        <div>
-            <Dialog
-            open={open}
-            onClose={handleClose}
-            aria-labelledby="alert-dialog-title"
-            aria-describedby="alert-dialog-description"
-            >
-            <DialogTitle id="alert-dialog-title">{t('Participation.DeleteActivity')}</DialogTitle>
-            <DialogContent>
-                <DialogContentText id="alert-dialog-description">
-                {t('Participation.DeleteMessageConfirm')} <br/>
-                {t('Participation.ActivityDescription.Label')} : <strong>{participation?.description}</strong> <br/>
-                {t('Participation.DateWithoutAsterisk')} : <strong>{formatDate(participation?.date)}</strong> 
-                </DialogContentText>
-            </DialogContent>
-            <DialogActions>
-                <Button onClick={handleClose} autoFocus>{t('Common.KeepIt')} </Button>
-                <Button onClick={handleDelete}>{t('Common.Delete')}</Button>
-            </DialogActions>
-            </Dialog>
-        </div>
-    );
-}
+  return (
+    <div>
+      <Dialog
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="alert-dialog-title"
+        aria-describedby="alert-dialog-description"
+      >
+        <DialogTitle
+          className="text-darkblue-800"
+          sx={{ fontSize: '1.4rem', fontWeight: '700', marginTop: 2 }}
+          id="alert-dialog-title"
+        >
+          {t('Participation.DeleteActivity')}
+        </DialogTitle>
+        <DialogContent>
+          <DialogContentText
+            id="alert-dialog-description"
+            sx={{ color: '#2F3940', fontWeight: '600', WebkitFontSmoothing: 'antialiased' }}
+          >
+            {t('Participation.DeleteMessageConfirm')} <br />
+            {t('Participation.ActivityDescription.Label')} : <strong>{participation?.description}</strong> <br />
+            {t('Participation.DateWithoutAsterisk')} : <strong>{formatDate(participation?.date)}</strong>
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions sx={{ marginBottom: 2, marginRight: 2 }}>
+          <Button variant="outlined" size="large" onClick={handleClose} autoFocus sx={{ marginRight: 1 }}>
+            {t('Common.KeepIt')}{' '}
+          </Button>
+          <Button className="!bg-red hover:!bg-[#B11B24]" variant="contained" size="large" onClick={handleDelete}>
+            {t('Common.Delete')}
+          </Button>
+        </DialogActions>
+      </Dialog>
+    </div>
+  );
+};
