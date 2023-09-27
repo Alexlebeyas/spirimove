42b83a923a10c97fb96fbf4f5f5ffa96d2022043
Alexis Mouloum <>
2023-09-27 14:47:49 -0400

    fix issue on permissions param

--- backend/spiri_move/admin_panel_permissions.py
+++ backend/spiri_move/admin_panel_permissions.py
@@ -3,25 +3,30 @@ class ModerationPermissions:
         Moderator permission in admin board.
         Administrator has all moderator permissions
     """
-    def has_delete_permission(self, request):
+
+    # pylint: disable=unused-argument
+    def has_delete_permission(self, request, obj=None):
         """
             Delete permissions for moderator and admin
         """
         return False
 
-    def has_add_permission(self, request):
+    # pylint: disable=unused-argument
+    def has_add_permission(self, request, obj=None):
         """
             Add permissions for moderator and admin
         """
         return False
 
-    def has_change_permission(self, request):
+    # pylint: disable=unused-argument
+    def has_change_permission(self, request, obj=None):
         """
             Edit permissions for moderator and admin
         """
         return (request.user.is_moderator() or request.user.is_admin()) if request.user.is_authenticated else False
 
-    def has_module_permission(self, request):
+    # pylint: disable=unused-argument
+    def has_module_permission(self, request, obj=None):
         """
             List permissions for moderator and admin
         """
@@ -32,13 +37,14 @@ class AdminPermissions(ModerationPermissions):
     """
         Administrator permission in admin board.
     """
-    def has_change_permission(self, request):
+
+    def has_change_permission(self, request, obj=None):
         """
             Edit permissions for admin only
         """
         return request.user.is_admin() if request.user.is_authenticated else False
 
-    def has_module_permission(self, request):
+    def has_module_permission(self, request, obj=None):
         """
             List permissions for admin only
         """
