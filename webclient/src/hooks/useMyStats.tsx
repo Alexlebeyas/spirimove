601dd654c701dc6f5025d9d3fab9fca77eb8f5e4
Praveen Venigalla <praveenv.aiesec@gmail.com>
2023-10-02 00:32:43 -0400

    SM-173 - Refresh stats when any participation log is deleted

--- webclient/src/hooks/useMyStats.tsx
+++ webclient/src/hooks/useMyStats.tsx
@@ -1,25 +1,32 @@
+import { useState, useEffect, useCallback } from 'react';
 import { IMyStats } from '@/interfaces/IMyStats';
 import { IContest } from '@/interfaces/contest';
 import ApiService from '@/services/ApiService';
-import { useState, useEffect } from 'react';
+
+const INITIAL_STATS: IMyStats = {
+  contest_name: '',
+  nb_point: 0,
+  nb_days: 0,
+  streak: 0,
+};
 
 export const useMyStats = (contest: IContest | undefined) => {
-  const [stats, setStats] = useState<IMyStats>({
-    contest_name: '',
-    nb_point: 0,
-    nb_days: 0,
-    streak: 0,
-  });
+  const [stats, setStats] = useState<IMyStats>(INITIAL_STATS);
 
-  useEffect(() => {
-    const getStats = async () => {
-      const myStats = (await ApiService.get(`my/stats/${contest?.id}`)).data;
-
-    setStats(myStats);
-  };
-
-    if (contest) getStats();
+  const getStats = useCallback(async () => {
+    if (!contest) return;
+    try {
+      const myStatsRes = await ApiService.get(`my/stats/${contest.id}`);
+      const data = myStatsRes.data;
+      setStats(data);
+    } catch (error) {
+      console.error('Error fetching stats:', error);
+    }
   }, [contest]);
 
-  return stats;
+  useEffect(() => {
+    getStats();
+  }, [getStats]);
+
+  return { stats, refreshStats: getStats };
 };
